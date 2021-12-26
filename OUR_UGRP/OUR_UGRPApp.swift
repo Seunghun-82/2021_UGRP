4f7fb54e32244a297fda186a1cb5db59fe082b04
Seunghun-82 <useful0512@naver.com>
2021-12-26 16:30:46 +0900

    Initial Commit

---
+++ OUR_UGRP/OUR_UGRPApp.swift
@@ -0,0 +1,17 @@
+//
+//  OUR_UGRPApp.swift
+//  OUR_UGRP
+//
+//  Created by seunghun on 2021/12/26.
+//
+
+import SwiftUI
+
+@main
+struct OUR_UGRPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
